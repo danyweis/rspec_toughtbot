d0d897d6eda9dd3387b17690739e1ae562f3442e
danyweis <danyweismail@gmail.com>
2022-10-21 23:56:18 +0700

    Allow user to create a todo

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,5 +1,26 @@
 class TodosController < ApplicationController
   def index
-  
+    @todos = Todo.all
   end 
+
+  def new 
+    @todo = Todo.new
+  end
+
+  def create
+    # redirect_to todos_path
+    
+    @todo = Todo.new(set_todo_params)
+    if @todo.save
+      redirect_to todos_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def set_todo_params
+    params.require(:todo).permit(:title)
+  end
+
 end
---
+++ spec/features/user_creates_todos_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+feature "User creates todo" do
+  scenario "successfully" do
+    visit root_path
+    
+    click_on "Add a new todo"
+    fill_in 'Title', with: 'Learn RSpec'
+    click_on "Submit"
+
+    expect(page).to have_css "li, .todos", text: "Learn RSpec"
+    
+  end
+end
