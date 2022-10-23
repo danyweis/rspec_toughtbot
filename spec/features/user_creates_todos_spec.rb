11c1872d8c959592aa1d756ef1baebc18ea03931
danyweis <danyweismail@gmail.com>
2022-10-23 23:32:57 +0700

    User can sign in

--- spec/features/user_creates_todos_spec.rb
+++ spec/features/user_creates_todos_spec.rb
@@ -2,8 +2,9 @@ require 'rails_helper'
 
 feature "User creates todo" do
   scenario "successfully" do
-    visit root_path
-    
+    # visit root_path
+    sign_in
+
     click_on "Add a new todo"
     fill_in 'Title', with: 'Learn RSpec'
     click_on "Submit"
